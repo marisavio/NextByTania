cf6b66107458fad024519d4ab16e1befcc868a6c
Mariana Savio <marianasavio16@gmail.com>
2024-04-27 22:46:29 -0300

    Adicionar conteudo v2

--- script.js
+++ script.js
@@ -1,22 +1,70 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var header = document.getElementById("home");
-    var sticky = header.offsetTop;
-  
-    function myFunction() {
-      if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-      } else {
-        header.classList.remove("sticky");
-      }
-    }
-  
-    window.onscroll = function() {
-      myFunction();
-    };
-
     document.getElementById("home-button").addEventListener("click", function() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
+
+    document.getElementById("home-button2").addEventListener("click", function() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      });
+
+    var facialModal = document.getElementById("facial-modal");
+    var bodyModal = document.getElementById("body-modal");
+    var hairModal = document.getElementById("hair-modal");
+
+    var btnFacial = document.getElementById("btn-facial-modal");
+    var btnBody = document.getElementById("btn-body-modal");
+    var btnHair = document.getElementById("btn-hair-modal");
+
+    var finalizedModal = document.getElementById("finalized-modal");
+    var btnFinalize = document.getElementById("btn-finalize");
+
+    
+    var spanFacial = document.getElementsByClassName("closeFacial")[0];
+    var spanBody = document.getElementsByClassName("closeBody")[0];
+    var spanHair = document.getElementsByClassName("closeHair")[0];
+    
+    btnFinalize.onclick = function() {
+      finalizedModal.style.display = "block";
+    }
+
+    btnFacial.onclick = function() {
+      facialModal.style.display = "block";
+    }
+    
+    btnBody.onclick = function() {
+      bodyModal.style.display = "block";
+    }
+
+    btnHair.onclick = function() {
+      hairModal.style.display = "block";
+    }
+
+    spanFacial.onclick = function() {
+      facialModal.style.display = "none";
+    }
+    
+    spanBody.onclick = function() {
+      bodyModal.style.display = "none";
+    }
+    
+    spanHair.onclick = function() {
+      hairModal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == facialModal) {
+        facialModal.style.display = "none";
+      }
+
+      if (event.target == bodyModal) {
+        bodyModal.style.display = "none";
+      }
+
+      if (event.target == hairModal) {
+        hairModal.style.display = "none";
+      }
+    } 
   });
   
